a719f31bde4b54ce623ac810309ce008039002c7
Saecki <tobiasschmitz2001@gmail.com>
2020-04-05 03:23:12 +0200

    update

---
+++ other/rustup.sh
@@ -0,0 +1,1 @@
+sudo -H -u $INSTALL_USER sh -c 'curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs > "$HOME"/temp; sh $HOME/temp -y'
--- other/simple-terminal.sh
+++ other/simple-terminal.sh
@@ -1,2 +1,2 @@
 git clone https://bitbucket.org/saecki/st.git /usr/local/src/st
-recompile-st
+make clean install -C /usr/local/src/st
