ff37570caf0051886b184744e7a02696f6333d3c
Tobias <tobi@pc.fritz.box>
2020-04-06 01:45:15 +0200

    update

--- scripts/teamspeak-3.sh
+++ scripts/teamspeak-3.sh
@@ -1,4 +1,9 @@
-url=$(curl https://www.teamspeak.com/versions/client.json| jq -r '.linux.x86_64.mirrors."teamspeak.com"')
+info=$(curl https://www.teamspeak.com/versions/client.json | jq -r '.linux.x86_64')
+version=$(echo $info | jq -r '.version')
+url=$(echo $info | jq -r '.mirrors."teamspeak.com"')
 wget $url
-chmod +x TeamSpeak3-Client-linux_amd64*.run
-./TeamSpeak3-Client-linux_amd64*.run
+chmod +x TeamSpeak3-Client-linux_amd64-$version.run
+./TeamSpeak3-Client-linux_amd64-$version.run
+mv TeamSpeak3-Client-linux_amd64 /opt
+/usr/local/bin/create-desktop-file -e /opt/TeamSpeak3-Client-linux_amd64/ts3client_runscript.sh -n "TeamSpeak 3"
+rm TeamSpeak3-Client-linux_amd64-$version.run
