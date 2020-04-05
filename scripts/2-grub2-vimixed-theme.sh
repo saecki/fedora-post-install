009bca5d0e8e240f7aa9c5c6d6c09d6a0147bf55
Tobias <tobi@pc.fritz.box>
2020-04-06 00:40:18 +0200

    update

---
+++ scripts/2-grub2-vimixed-theme.sh
@@ -0,0 +1,3 @@
+git clone https://saecki@bitbucket.org/saecki/vimixed.git /boot/grub2/themes/vimixed
+echo "GRUB_THEME=/boot/grub2/themes/vimixed/theme.txt" >> /etc/default/grub
+
---
+++ scripts/grub2-config.sh
@@ -0,0 +1,2 @@
+ln -s /boot/efi/EFI/fedora/grub.cfg /boot/grub2/
+/usr/local/bin/update-grub
