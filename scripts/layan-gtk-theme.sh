ca0b97cce8d9ce10754d44420140e03012d4873a
Saecki <tobiasschmitz2001@gmail.com>
2020-04-06 18:04:05 +0200

    update

--- scripts/layan-gtk-theme.sh
+++ scripts/layan-gtk-theme.sh
@@ -1,5 +1,6 @@
-git clone https://github.com/vinceliuice/Layan-gtk-theme.git
-Layan-gtk-theme/install.sh
+git clone https://github/com/vinceliuice/Layan-gtk-theme/git
+Layan-gtk-theme/install/sh
 rm -rf Layan-gtk-theme
-gsettings set org.gnome.shell.extensions.user-theme name "Layan-dark-solid"
-gsettings set org.gnome.desktop.interface icon-theme "Layan-dark-solid"
+sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.desktop.interface gtk-theme Layan-dark-solid
+sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.shell.extensions.user-theme name Layan-dark-solid
+
