8e9917439eaece306e01a1835d001e0a6c9b69ad
Tobias <tobi@pc.fritz.box>
2020-04-07 20:58:27 +0200

    update

--- scripts/st.sh
+++ scripts/st.sh
@@ -1,3 +1,8 @@
 git clone https://github.com/LukeSmithxyz/st.git /usr/local/src/st
 make clean install -C /usr/local/src/st
-/usr/local/bin/create-desktop-file -e /usr/local/bin/st -n st -g org.gnome.Terminal
+/usr/local/bin/create-desktop-file -e /usr/local/bin/launch-st -n st -g org.gnome.Terminal
+
+sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-st/ name 'launch-st'
+sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-st/ command 'launch-st'
+sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-st/ binding '<Super>Return'
+sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-st/']"
