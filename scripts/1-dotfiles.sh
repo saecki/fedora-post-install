489eb309493fb8cc4433b7eca6aabb42c2e680ac
Saecki <tobiasschmitz2001@gmail.com>
2020-04-11 03:47:45 +0200

    update

--- scripts/1-dotfiles.sh
+++ scripts/1-dotfiles.sh
@@ -1,2 +1,5 @@
 git clone --bare https://saecki@bitbucket.org/saecki/.dotfiles.git /home/"$INSTALL_USER"/.dotfiles
 git --work-tree=/home/"$INSTALL_USER" --git-dir=/home/"$INSTALL_USER"/.dotfiles checkout -f
+
+/usr/local/bin/playlist_localizer -g zsh -o /usr/share/zsh/site-functions/
+/usr/local/bin/playlist_localizer -g bash -o /etc/bash_completion.d/
