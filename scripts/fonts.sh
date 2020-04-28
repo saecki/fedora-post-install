6dc30803d1fa20b86631d5a074c13af93ae87972
Saecki <tobiasschmitz2001@gmail.com>
2020-04-28 16:18:26 +0200

    update

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,4 +1,3 @@
-
 sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.desktop.interface font-name "Noto Sans CJK HK 11"
 sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.desktop.interface document-font-name "Noto Sans CJK HK 11"
 sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.desktop.interface monospace-font-name "Monospace 10"
