6b7a2205e81f15a074014be8125703865bd2b4c5
Saecki <tobiasschmitz2001@gmail.com>
2020-05-10 17:56:38 +0200

    update

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -1,9 +1,19 @@
-sudo -Hu $INSTALL_USER git clone https://github.com/alacritty/alacritty.git $HOME/Documents/IdeaProjects/alacritty
+sudo -Hu $INSTALL_USER git clone https://github.com/alacritty/alacritty.git /home/$INSTALL_USER/Documents/IdeaProjects/alacritty
 
-sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-st/ name 'launch-terminal'
-sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-st/ command 'alacritty -t "magic box"'
-sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-st/ binding '<Super>Return'
+sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-terminal/ name 'launch-terminal'
+sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-terminal/ command 'env WAYLAND_DISPLAY= alacritty -t "magic box"'
+sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-terminal/ binding '<Super>Return'
 
 oldkeybindings=$(sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings)
-keybindings="${oldkeybindings::-1}, '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-terminal/']"
+
+commas="${oldkeybindings//[^\']}"
+amount="${#commas}"
+
+if [[ $amount == "0" ]]; then
+    oldkeybindings="["
+else
+    oldkeybindings="${oldkeybindings::-1}, "
+fi
+
+keybindings="$oldkeybindings'/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-terminal/']"
 sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "$keybindings"
