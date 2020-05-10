6bab3415319031ba1127b2631b2c29c919a0665b
Tobias <tobiasschmitz2001@gmail.com>
2020-05-10 05:42:04 +0200

    update

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -1,5 +1,4 @@
-git clone https://github.com/alacritty/alacritty.git /usr/local/src/alacritty
-
+sudo -Hu $INSTALL_USER git clone https://github.com/alacritty/alacritty.git $HOME/Documents/IdeaProjects/alacritty
 
 sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-st/ name 'launch-terminal'
 sudo -Hu $INSTALL_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $INSTALL_USER)/bus gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/launch-st/ command 'alacritty -t "magic box"'
