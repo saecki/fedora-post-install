95280badb9269ec1fc6e6bd66d06ff87c59def06
Saecki <tobiasschmitz2001@gmail.com>
2020-08-02 15:46:43 +0200

    update

--- scripts/free-file-sync.sh
+++ scripts/free-file-sync.sh
@@ -7,7 +7,7 @@ install() {
     
     sudo rm -rf /opt/FreeFileSync
 
-    wget -P $dl_dir "https://freefilesync.org/download/FreeFileSync_"$vers"_Linux.tar.gz"
+    wget --no-ftps-implicit -P $dl_dir "https://freefilesync.org/download/FreeFileSync_"$vers"_Linux.tar.gz"
     sudo tar xzf "$dl_dir/FreeFileSync_"$vers"_Linux.tar.gz" -C /opt
     
     rm -rf $dl_dir
