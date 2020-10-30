95ec3ef651b1294cf85eec7e02cb7c762e5920e6
Saecki <tobiasschmitz2001@gmail.com>
2020-10-30 14:29:18 +0100

    update

--- scripts/powerlevel10k.sh
+++ scripts/powerlevel10k.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+p10kpath="$HOME/.config/zsh/powerlevel10k"
+
 install() {
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $p10kpath
 }
 
 update() {
-    git -C $HOME/.oh-my-zsh/custom/themes/powerlevel10k pull
+    git -C $p10kpath pull
 }
 
 . "$(dirname $(dirname $(realpath $0)))/util/manage.sh"
