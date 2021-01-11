38292377a31dc0e74a7538b5956a29bef0fb9d9a
saecki <tobiasschmitz2001@gmail.com>
2021-01-11 09:13:53 +0100

    add discordptb script

---
+++ scripts/discordptb.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+install() {
+    dl_dir=$(mktemp -d)
+
+    wget -P $dl_dir https://discordapp.com/api/download/ptb\?platform\=linux\&format\=tar.gz
+
+    sudo rm -rf /opt/DiscordPTB
+    sudo tar xzf "$dl_dir/ptb?platform=linux&format=tar.gz" -C /opt/
+
+    rm -rf $dl_dir
+
+    sudo /usr/local/bin/create-desktop-file \
+	-e /opt/DiscordPTB/DiscordPTB \
+	-n DiscordPTB \
+	-g com.discordapp.Discord
+}
+
+update() {
+    echo "Let discord update itself or reinstall it"
+}
+
+. "$(dirname $(dirname $(realpath $0)))/util/manage.sh"
