4b496675174a610d45c107436f9a85d1a52e3170
saecki <tobiasschmitz2001@gmail.com>
2021-02-15 00:27:54 +0100

    add extension settings

--- util/settings.sh
+++ util/settings.sh
@@ -4,10 +4,19 @@ for file in $@; do
     if [[ $file == *.gsettings ]]; then
         echo -e "\napplying settings from $file:"
 
+        schemadir=""
         while read line; do
-            if [[ $line != "" ]] && [[ $line != "#"* ]]; then
-                echo "setting: $line"
-                gsettings set $line
+            if [[ $line == schemadir:* ]]; then 
+                schemadir=$(eval "echo ${line#schemadir:}")
+                echo "custom schemadir: $schemadir"
+            elif [[ "$line" != "" ]] && [[ $line != "#"* ]]; then
+                echo "$line"
+
+                if [[ $schemadir != "" ]]; then
+                    gsettings --schemadir $schemadir set $line
+                else
+                    gsettings set $line
+                fi
             fi
         done < $file
 
