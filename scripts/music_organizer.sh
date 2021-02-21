69f0c74efad9f53c7e4a8551abbba9bedc6bd04a
Tobias <tobi@localhost.localdomain>
2021-02-21 16:49:07 +0100

    update

--- scripts/music_organizer.sh
+++ scripts/music_organizer.sh
@@ -2,17 +2,17 @@
 
 install() {
     dl_dir=$(mktemp -d)
-    src_path="$dl_dir/playlist_localizer"
+    src_path="$dl_dir/music_organizer"
 
-    git clone https://github.com/saecki/playlist_localizer "$src_path"
+    git clone https://github.com/saecki/music_organizer "$src_path"
 
     "$HOME/.cargo/bin/cargo" build \
 	--manifest-path="$src_path/Cargo.toml" \
 	--release
 
     sudo cp \
-	"$src_path/target/release/playlist_localizer" \
-	/usr/local/bin/
+	"$src_path/target/release/cli" \
+	/usr/local/bin/music_organizer
 }
 
 update() {
---
+++ scripts/musicbrainz_plugins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+install() {
+    dl_dir=$(mktemp -d)
+    src_path="$dl_dir/musicbrainz_plugins"
+
+    git clone https://github.com/saecki/musicbrainz_plugins "$src_path"
+
+    (cd "$src_path" && ./install.sh)
+}
+
+update() {
+    install
+}
+
+. "$(dirname $(dirname $(realpath $0)))/util/manage.sh"
