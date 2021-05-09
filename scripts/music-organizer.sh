ee1eb17f2629b8d812ba82bee0a4a1acc86a016b
Saecki <tobiasschmitz2001@gmail.com>
2021-05-09 22:33:44 +0200

    rename music_organizer to music-organizer

--- scripts/music-organizer.sh
+++ scripts/music-organizer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-src_path="$HOME/Projects/music_organizer"
+src_path="$HOME/Projects/music-organizer"
 
 compile() {
     (cd "$src_path" && env SHELL=/usr/bin/zsh ./install.sh)
 }
 
 install() {
-    git clone git@github.com:Saecki/music_organizer "$src_path"
+    git clone git@github.com:Saecki/music-organizer "$src_path"
 
     compile
 }
