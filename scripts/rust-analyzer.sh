097bdc346ef75de2e86452c0ac76fb8d450a0a4c
Saecki <tobiasschmitz2001@gmail.com>
2021-07-19 19:58:34 +0200

    add rust-analyzer script

---
+++ scripts/rust-analyzer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+bin_url="https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz"
+bin_path="$HOME/.local/bin/rust-analyzer"
+
+install() {
+    curl -L "$bin_url" | gunzip -c - > "$bin_path"
+}
+
+update() {
+    rm "$bin_path"
+    install
+}
+
+. "$(dirname $(dirname $(realpath $0)))/util/manage.sh"
