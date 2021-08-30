a83cf80f243b93093ee0d6caf8cb55c1543fcbf5
Saecki <tobiasschmitz2001@gmail.com>
2021-08-30 17:30:37 +0200

    add lua-language-server

---
+++ scripts/headless/lua-language-server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+src_path="$HOME/Projects/lua-language-server"
+
+compile() {
+    (cd "$src_path/3rd/luamake" && ./compile/install.sh)
+    (cd "$src_path" && ./3rd/luamake/luamake rebuild)
+}
+
+install() {
+    git clone --recursive git@github.com:sumneko/lua-language-server "$src_path"
+
+    compile
+}
+
+update() {
+    git -C "$src_path" pull origin master
+    git -C "$src_path" submodule update --init --recursive
+
+    compile
+}
+
+. "$(dirname $(dirname $(dirname $(realpath $0))))/util/manage.sh"
+
