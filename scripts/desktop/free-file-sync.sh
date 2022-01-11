b703dca14497bc7defbd7a426596cf89a103e3f7
Saecki <tobiasschmitz2001@gmail.com>
2022-01-11 04:27:00 +0100

    feat(scripts/FreeFileSync): check if version is up to date

--- scripts/desktop/free-file-sync.sh
+++ scripts/desktop/free-file-sync.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-install() {
+fetch_version() {
     vers=$(curl https://freefilesync.org/download.php | rg --no-filename \
         -e "^(.*?)FreeFileSync_(.*?)_Linux.tar.gz(.*?)$" -r '$2')
-    if [[ $vers == "" ]]; then
-        echo "unable to get version"
-        exit 1
-    else
-        echo "got version $vers"
-    fi
+    echo "$vers"
+}
 
+_install() {
+    vers="$1"
     dl_dir=$(mktemp -d)
 
     sudo rm -rf /opt/FreeFileSync
@@ -29,8 +27,43 @@ install() {
     /;}")
 }
 
+install() {
+    vers=$(fetch_version)
+    if [[ $vers == "" ]]; then
+        echo -e "\nunable to fetch version"
+        exit 1
+    else
+        echo -e "\ninstalling version $vers\n"
+    fi
+
+    _install "$vers"
+}
+
 update() {
-    echo "Let FreeFileSync update itself or reinstall it"
+    if [[ -e /opt/FreeFileSync/CHANGELOG ]]; then
+        line=$(head -n 1 /opt/FreeFileSync/CHANGELOG)
+        pat="^FreeFileSync (.*) \[[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}\]"
+        if [[ "$line" =~ $pat ]]; then
+            vers=$(fetch_version)
+            if [[ $vers == "" ]]; then
+                echo -e "\nunable to fetch version"
+                exit 1
+            fi
+            
+            if [[ "${BASH_REMATCH[1]}" != "$vers" ]]; then
+                echo -e "\ninstalling version $vers\n"
+                _install "$vers"
+            else
+                echo -e "\nversion $vers is already installed"
+            fi
+        else
+            echo "couldn't read installed version"
+            exit 1
+        fi
+    else
+        echo -e "\nFreeFileSync isn't installed, installing ...\n"
+        install
+    fi
 }
 
 . "$(dirname $(dirname $(dirname $(realpath $0))))/util/manage.sh"
