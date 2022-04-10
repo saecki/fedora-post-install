e6a01b914f1bb8e5e0e8f34515d72ab482c5dfa9
Saecki <tobiasschmitz2001@gmail.com>
2022-04-10 14:43:19 +0200

    refactor: install rust toolchain using rustup
    This reverts commit ed527242a1228b478f9d9e474acd14a65a2245a6.

---
+++ scripts/headless/1-rustup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+completions() {
+    "$HOME/.cargo/bin/rustup" completions zsh rustup > "$ZDOTDIR/functions/_rustup"
+    "$HOME/.cargo/bin/rustup" completions zsh cargo > "$ZDOTDIR/functions/_cargo"
+}
+
+install() {
+    dl_dir=$(mktemp -d)
+
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > "$dl_dir/rustup.sh"
+    chmod +x "$dl_dir/rustup.sh"
+
+    "$dl_dir/rustup.sh" --no-modify-path -y
+    completions
+}
+
+update() {
+    "$HOME/.cargo/bin/rustup" update
+    completions
+}
+
+. "$(dirname $(dirname $(dirname $(realpath $0))))/util/manage.sh"
+
