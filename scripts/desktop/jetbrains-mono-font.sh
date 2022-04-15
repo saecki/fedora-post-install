53c5c7136c9431980011886e8677e317a1e4a622
Saecki <tobiasschmitz2001@gmail.com>
2022-04-15 22:15:29 +0200

    feat: update jetbrains-mono-font script

--- scripts/desktop/jetbrains-mono-font.sh
+++ scripts/desktop/jetbrains-mono-font.sh
@@ -4,10 +4,17 @@ install() {
     dl_dir=$(mktemp -d)
 
     wget -O "$dl_dir/jbmono.zip" https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+    unzip "$dl_dir/jbmono.zip" -d "$dl_dir/jbmono"
 
-    rm -rf ~/.local/share/fonts/JetBrainsMono
-    mkdir -p ~/.local/share/fonts
-    unzip "$dl_dir/jbmono" -d ~/.local/share/fonts/JetBrainsMono
+    git clone git@github.com:saecki/corners "$dl_dir/corners" 
+
+    rm -rf "$HOME/.local/share/fonts/JetBrainsMono/*"
+    mkdir -p "$HOME/.local/share/fonts/JetBrainsMono"
+
+    INPUT_FILE="$dl_dir/jbmono/JetBrains Mono Regular Nerd Font Complete.ttf"
+    OUTPUT_FILE="$HOME/.local/share/fonts/JetBrainsMono/JetBrains Mono Regular Nerd Font Complete.ttf"
+
+    (cd "$dl_dir/corners" && $dl_dir/corners/patch "$INPUT_FILE" "$OUTPUT_FILE")
 }
 
 update() {
