b9bc5473e63b3c8ebb5be90055f4bf322aeb9aef
Saecki <tobiasschmitz2001@gmail.com>
2024-01-14 22:10:06 +0100

    feat(scripts): update jetbrains mono font

--- scripts/desktop/jetbrains-mono-font.sh
+++ scripts/desktop/jetbrains-mono-font.sh
@@ -3,7 +3,7 @@
 install() {
     dl_dir=$(mktemp -d)
 
-    wget -O "$dl_dir/jbmono.zip" https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip
+    wget -O "$dl_dir/jbmono.zip" https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
     unzip "$dl_dir/jbmono.zip" -d "$dl_dir/jbmono"
 
     git clone git@github.com:saecki/corners "$dl_dir/corners"
@@ -11,8 +11,9 @@ install() {
     rm -rf "$HOME/.local/share/fonts/JetBrainsMono/*"
     mkdir -p "$HOME/.local/share/fonts/JetBrainsMono"
 
-    INPUT_FILE="$dl_dir/jbmono/JetBrains Mono Regular Nerd Font Complete.ttf"
-    OUTPUT_FILE="$HOME/.local/share/fonts/JetBrainsMono/JetBrains Mono Regular Nerd Font Complete.ttf"
+    FILENAME="JetBrainsMonoNerdFont-Regular.ttf"
+    INPUT_FILE="$dl_dir/jbmono/$FILENAME"
+    OUTPUT_FILE="$HOME/.local/share/fonts/JetBrainsMono/$FILENAME"
 
     (cd "$dl_dir/corners" && $dl_dir/corners/patch "$INPUT_FILE" "$OUTPUT_FILE")
 }
