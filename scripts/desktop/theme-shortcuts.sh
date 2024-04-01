11f87b9328b10277b2bbb108e8072a73e4794b94
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2024-04-01 14:27:33 +0200

    feat(scripts): add reload-theme shortcut

--- scripts/desktop/theme-shortcuts.sh
+++ scripts/desktop/theme-shortcuts.sh
@@ -12,22 +12,35 @@ install() {
 
     keybindings="$oldkeybindings"
 
-    keybinding="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/toggle-theme/"
-    if [[ $keybindings != *"$keybinding"* ]]; then
+    togglekeybinding="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/toggle-theme/"
+    if [[ $keybindings != *"$togglekeybinding"* ]]; then
         echo "adding toggle-theme keybinding"
-        keybindings="$keybindings'$keybinding',"
+        keybindings="$keybindings'$togglekeybinding',"
     else
         echo "toggle-theme keybinding already present, updating it"
+    fi
+
+    reloadkeybinding="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/reload-theme/"
+    if [[ $keybindings != *"$reloadkeybinding"* ]]; then
+        echo "adding reload-theme keybinding"
+        keybindings="$keybindings'$reloadkeybinding',"
+    else
+        echo "reload-theme keybinding already present, updating it"
     fi
 
     keybindings="${keybindings::-1}]"
 
     gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "$keybindings"
 
-    keybindingpath="org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$keybinding"
+    keybindingpath="org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$togglekeybinding"
     gsettings set $keybindingpath name "'Toggle theme'"
     gsettings set $keybindingpath command "'$HOME/.local/bin/theme toggle'"
     gsettings set $keybindingpath binding "'<Super>t'"
+
+    keybindingpath="org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$reloadkeybinding"
+    gsettings set $keybindingpath name "'Reload theme'"
+    gsettings set $keybindingpath command "'$HOME/.local/bin/theme reload'"
+    gsettings set $keybindingpath binding "'<Super>r'"
 }
 
 update() {
