a3b59ff79f3bb1b026caf9dd483ddf54b1866860
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-09-21 18:03:02 +0200

    feat(scripts)!: remove broken update check for free-file-sync

--- scripts/desktop/free-file-sync.sh
+++ scripts/desktop/free-file-sync.sh
@@ -40,38 +40,7 @@ install() {
 }
 
 update() {
-    if [[ -e /opt/FreeFileSync/CHANGELOG ]]; then
-        line=$(head -n 1 /opt/FreeFileSync/CHANGELOG)
-        pat="^FreeFileSync (.*) \[[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}\]"
-        if [[ "$line" =~ $pat ]]; then
-            local_vers="${BASH_REMATCH[1]}"
-            if [[ $local_vers == "" ]]; then
-                echo -e "\nunable to read local version"
-                exit 1
-            fi
-
-            newest_vers=$(fetch_version)
-            if [[ $newest_vers == "" ]]; then
-                echo -e "\nunable to fetch version"
-                exit 1
-            fi
-            echo "Local version: '$local_vers'"
-            echo "Newest version: '$newest_vers'"
-
-            if [[ "$local_vers" != "$newest_vers" ]]; then
-                echo -e "\ninstalling version $newest_vers\n"
-                download "$newest_vers"
-            else
-                echo -e "\nversion $local_vers is already installed"
-            fi
-        else
-            echo "couldn't read installed version"
-            exit 1
-        fi
-    else
-        echo -e "\nFreeFileSync isn't installed, installing ...\n"
-        install
-    fi
+    echo "Reinstall FreeFileSync manually"
 }
 
 . "$(dirname $(dirname $(dirname $(realpath $0))))/util/manage.sh"
